75d43a757edce0b312c5cf125816a2089beb02fa
InguCell <mathiaslawson70@gmail.com>
2022-07-29 12:02:13 +0000

    nav full responsiveness for required small screens

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar/Navbar";
-import  Footer from "./components/footer/footer";
+import  Footer from "./components/Footer/Footer";
 
 function App() {
   return (
 
     <>
       <Navbar />
-      <></>
       <Footer/>
     </>
   );
