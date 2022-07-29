e00d10d322d5c14b8ed15e9b0f9484cf72f4e14d
Hancho7 <litorojose1@gmail.com>
2022-07-29 14:47:32 +0000

    contact form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar/Navbar";
-<<<<<<< HEAD
 import  Footer from "./components/footer/footer";
 import Contact from "./components/contact-page/contact";
-=======
-import  Footer from "./components/Footer/Footer";
->>>>>>> af994452c06c2a286746031b178d673d002e99cf
+// import  Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
 
     <>
       <Navbar />
-<<<<<<< HEAD
+
       <></>
       <></>
       <Contact/>
       <></>
       <></>
-=======
->>>>>>> af994452c06c2a286746031b178d673d002e99cf
       <Footer/>
     </>
   );
