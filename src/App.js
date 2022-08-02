9e6e426de10c3920b6a94c3d1c86394339b23b04
InguCell <mathiaslawson70@gmail.com>
2022-08-02 02:30:25 +0000

    landing partial part 1

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar/Navbar";
 import  Footer from "./components/Footer/Footer";
 import Contact from "./components/contact-page/contact";
-// import  Footer from "./components/Footer/Footer";
+import Landing from  ".//routes/landing-page/LandingPage";
+import { BrowserRouter, Route, Router, Routes} from 'react-router-dom';
 
 
 function App() {
   return (
 
-    <>
-      <Navbar />
+    <div>
+    <Navbar />
+<BrowserRouter>
+<Routes>
+  <Route exact path="/" element={ <Landing /> }></Route>
+</Routes>
 
-      <></>
-      <></>
-      <Contact/>
-      <></>
-      <></>
+<Routes>
+  <Route path="/contact" element={ <Contact/> }></Route>
+</Routes>
+
+
+
+</BrowserRouter>
+
+
       <Footer/>
-    </>
+    </div>
   );
 }
 
--- src/routes/landing-page/LandingPage.js
+++ src/routes/landing-page/LandingPage.js
@@ -1,8 +1,35 @@
 import React from 'react'
-import girl_picture from './images/picture1.png';
+import girl_picture from './picture1.png';
+import { Container, Row, Col} from "react-bootstrap";
+import "./LandingPage.css"
+
 
 export default function landingpage() {
   return (
-    <div></div>
+    <div> 
+
+
+<Container fluid>
+      
+
+<div class="part1">
+<img class="picture1" src={girl_picture} /> 
+<div className='writing-container'>
+<h2 class="part1-writing">Fast, Secured and <br />Accessible Voting System</h2>
+
+<div class="part1-sub">
+Voting made easy for you. This website is designed to ensure<br />
+ a secured voting session for every student
+
+</div>
+
+</div>
+ 
+</div>
+
+    </Container>
+
+
+    </div>
   )
 }
