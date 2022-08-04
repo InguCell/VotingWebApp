8cbb26a1b2003f68f3de15f7636072db11f94707
InguCell <mathiaslawson70@gmail.com>
2022-08-04 23:40:19 +0000

    part2 parital responsiveness

--- src/components/routes/landing-page/Part2.js
+++ src/components/routes/landing-page/Part2.js
@@ -3,6 +3,8 @@ import "./Part2.css"
 import { Container , Col , Row, Card, Button} from 'react-bootstrap'
 import star from "./picture2.png"
 import secure from "./picture3.png"
+import news from "./picture4.png"
+import vote from "./picture5.png"
 
 export default function Part2() {
 //container ready
@@ -13,8 +15,8 @@ const part2 =
 
 <Row>
         <Col xs>
-
-        <Card className='cards' style={{border:""}}>
+<div className='card1-pos'>
+<Card className='cards cards1' style={{border:""}}>
       <Card.Img variant="top" src={star} className="star" />
       <div>
            <img src={secure} className='secure'></img>
@@ -24,16 +26,65 @@ const part2 =
         Secured System
             </h3></Card.Title>
         <Card.Text><p class="card-sub-heading">
-        With our system,your data is 
+        With our system your data is 
 safe and secured
         </p>
       
         </Card.Text>
       </Card.Body>
     </Card>
+    
+</div>
+       
         </Col>
-        <Col xs={{ order: 12 }}>Second, but last</Col>
-        <Col xs={{ order: 1 }}>Third, but second</Col>
+        <Col xs={{ order: 12 }}>
+
+            <div className='card2-pos'>
+            <Card className='cards cards2' style={{border:""}}>
+      <Card.Img variant="top" src={star} className="star" />
+      <div>
+           <img src={news} className='news secure'></img>
+      </div>
+      <Card.Body class="card-sub">
+        <Card.Title><h3 class="card-heading">
+        Vote Online
+            </h3></Card.Title>
+        <Card.Text><p class="card-sub-heading">
+        With just a few clicks you can vote for your preferred candidates
+        </p>
+      
+        </Card.Text>
+      </Card.Body>
+    </Card>
+            </div>
+
+        
+        </Col>
+        
+        <Col xs={{ order: 1 }}>
+
+            <div className='card3-pos'>
+            <Card className='cards cards3' style={{border:""}}>
+      <Card.Img variant="top" src={star} className="star star-alt3" />
+      <div>
+           <img src={vote} className='vote secure'></img>
+      </div>
+      <Card.Body class="card-sub">
+        <Card.Title><h3 class="card-heading">
+        Real Time Results
+            </h3></Card.Title>
+        <Card.Text><p class="card-sub-heading sub-alt3">
+        View real time votinf results and scores for each candidates
+        </p>
+      
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+            </div>
+
+        
+        </Col>
       </Row>
       
   </Container>
