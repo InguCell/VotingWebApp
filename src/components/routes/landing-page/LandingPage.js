283aa64f79e23c8d2f369e4dbadf5b0379527f64
Hancho7 <litorojose1@gmail.com>
2022-08-20 02:11:43 +0000

    full responsive footer

--- src/components/routes/landing-page/LandingPage.js
+++ src/components/routes/landing-page/LandingPage.js
@@ -3,8 +3,7 @@ import "./Part1.css"
 import Part1 from './Part1'
 import Part2 from './Part2'
 import Part3 from './Part3'
-import Part4 from './part4'
-import Part5 from './part5'
+
 
 
 export default function Landingpage() {
@@ -15,8 +14,6 @@ export default function Landingpage() {
           <Part1/>
           <Part2/>
           <Part3/>
-          <Part4/>
-          <Part5/>
     </div>
   )
 }
