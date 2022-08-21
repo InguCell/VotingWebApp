0ad087fae17fe7efaf8e88afeb3551aae27c36ee
inguCell <mathiaslawson70@gmail.com>
2022-08-21 12:03:21 +0000

    part4 80% responsiveness done

--- src/components/routes/landing-page/LandingPage.js
+++ src/components/routes/landing-page/LandingPage.js
@@ -3,6 +3,7 @@ import "./Part1.css"
 import Part1 from './Part1'
 import Part2 from './Part2'
 import Part3 from './Part3'
+import Part4 from './Part4'
 
 
 export default function Landingpage() {
@@ -13,6 +14,8 @@ export default function Landingpage() {
           <Part1 />
           <Part2 />
           <Part3 />
+          <Part4 />
+
     </div>
   )
 }
